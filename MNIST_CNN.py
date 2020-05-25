241dbb6c28bfe105af01ff88e837f661c5382d36
Pallavi Bhandari <44194875+PallaviBhandari13@users.noreply.github.com>
2020-05-25 22:12:57 +0530

    Update MNIST_CNN.py

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -1,32 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import tensorflow as tf
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-
-
-# In[2]:
-
-
-import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
-image_index = 7777 
-print(y_train[image_index]) 
-plt.imshow(x_train[image_index], cmap='Greys')
-
-
-# In[3]:
-
-
-x_train.shape
-
-
-# In[4]:
-
-
 from __future__ import print_function
 import keras
 from keras.datasets import mnist
@@ -34,40 +5,29 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 
+import tensorflow as tf
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-# In[6]:
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+image_index = 7777 
+print(y_train[image_index]) 
+plt.imshow(x_train[image_index], cmap='Greys')
 
+x_train.shape
 
 batch_size = 128
 num_classes = 10
 epochs = 12
 
-
-# In[7]:
-
-
-
-# input image dimensions
 img_rows, img_cols = 28, 28
-
-
-# In[8]:
-
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-
-# In[9]:
-
-
 x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
 x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
 input_shape = (img_rows, img_cols, 1)
-
-
-# In[10]:
-
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -77,17 +37,9 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-
-# In[11]:
-
-
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-
-
-# In[12]:
-
 
 #Add layers
 
@@ -103,16 +55,9 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
-
-# In[13]:
-
-
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
-
-
-# In[14]:
 
 
 #Fit the model
@@ -123,15 +68,9 @@ model.fit(x_train, y_train,
           validation_data=(x_test, y_test))
 
 
-# In[15]:
-
-
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-
-
-# In[22]:
 
 
 #Test the model
@@ -140,10 +79,5 @@ image_index = 3174
 plt.imshow(x_test[image_index].reshape(28, 28),cmap='Greys')
 pred = model.predict(x_test[image_index].reshape(1, img_rows, img_cols, 1))
 print(pred.argmax())
-
-
-# In[ ]:
-
-
 
 
