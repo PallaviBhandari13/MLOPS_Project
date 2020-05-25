b492362b7bcb26f64ca77ecb7e5ea046b8e47d8b
Pallavi Bhandari <44194875+PallaviBhandari13@users.noreply.github.com>
2020-05-25 22:27:55 +0530

    Update MNIST_CNN.py

--- MNIST_CNN.py
+++ MNIST_CNN.py
@@ -1,83 +1,39 @@
-import keras, sys
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+# Libraries
 from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
 from keras.utils import np_utils
-
-# Loading MNIST Dataset
-(x_train, y_train), (x_test, y_test)  = mnist.load_data()
-
-# Finding No. of Rows and Columns
-rows_of_img = x_train[0].shape[0]
-cols_of_img = x_train[1].shape[0]
-
-# store the shape of a single image 
-input_shape = (rows_of_img, cols_of_img, 1)
-
-# change our image type to float32 data type
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-
-# Featuring Scaling - Normalization
-x_train /= 255
-x_test /= 255
-
-# Doing One-Hot Encoding
+# load data
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+# flatten 28*28 images to a 784 vector for each image
+num_pixels = X_train.shape[1] * X_train.shape[2]
+X_train = X_train.reshape((X_train.shape[0], num_pixels)).astype('float32')
+X_test = X_test.reshape((X_test.shape[0], num_pixels)).astype('float32')
+# normalize inputs from 0-255 to 0-1
+X_train = X_train / 255
+X_test = X_test / 255
+# one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+# define baseline model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+# build the model
+model = baseline_model()
+# Fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=5, batch_size=200, verbose=0)
+# Final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy:  %.2f%%" % (scores[1]*100))
 
-n_classes = y_test.shape[1]
+model.save("model/mnist_model.h5")
 
-# Creating model
-model = Sequential()
-
-# Adding CRP layers
-model.add(Conv2D(20,kernel_size,padding="same",input_shape=input_shape))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-count = 1
-while count <= crp_count:
-    model.add(Conv2D(50,kernel_size,padding="same"))
-    model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
-    count+=1
-    
-# FC
-model.add(Flatten())
-
-count = 1
-while count <= fc_count:
-    model.add(Dense(500))
-    model.add(Activation("relu"))
-    count+=1
-    
-model.add(Dense(n_classes))
-model.add(Activation("softmax")) 
-
-model.compile(loss="categorical_crossentropy", optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
-
-print(model.summary())
-
-# Conerting Images to 4D
-x_train = x_train.reshape(x_train.shape[0], rows_of_img, cols_of_img, 1)
-x_test = x_test.reshape(x_test.shape[0], rows_of_img, cols_of_img, 1)
-
-# Training Parameters
-batch_size = batch_size_passed
-epochs = no_of_epochs
-
-history = model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          validation_data=(x_test, y_test), 
-          shuffle=True)
-
-model.save("mnist_LeNet.h5")   
-
-# Evaluating the accuracy
-scores = model.evaluate(x_test, y_test, verbose=1) 
-print("\nAccuracy is :-\n") 
-print(int(scores[1] * 100)) 
-
+file1 = open("result.txt","w")
+file1.write(str(scores[1]*100))
